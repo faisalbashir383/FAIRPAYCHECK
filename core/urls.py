2a3fff925ce8a0a4383d7d7222c28aec11799949
FaisalAhmad383 <faisal.ahmad@doctustech.com>
2025-12-25 02:01:22 +0530

    first commit

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,12 @@ Core app URL configuration.
 """
 
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path('', views.index_view, name='index'),
     path('api/calculate/', views.calculate_score_api, name='calculate_score'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'), name='robots'),
+    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml'), name='sitemap'),
 ]
